e916c6284b51c855e1bc200f980aa25b21dc4276
Jérémy Poullain <jeremy.poullain@atos.net>
2025-09-10 10:36:37 +0200

    Update page not found

--- frontend/src/app/core/pages/page-not-found/page-not-found.ts
+++ frontend/src/app/core/pages/page-not-found/page-not-found.ts
@@ -1,11 +1,13 @@
+import { HttpStatusCode } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   imports: [],
   templateUrl: './page-not-found.html',
-  styleUrl: './page-not-found.scss'
+  styleUrl: './page-not-found.scss',
 })
 export class PageNotFound {
-
+  public NOT_FOUND_ERROR_CODE: number = HttpStatusCode.NotFound;
+  public PAGE_NOT_FOUND_MESSAGE: string = $localize`Page not found`;
 }
